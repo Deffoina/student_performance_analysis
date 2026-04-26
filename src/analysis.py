204791a01d0c13d58305f7fe22d5e9623f70e1b2
Deffoina <inamoting001@gmail.com>
2026-04-26 15:59:28 +0200

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,42 +1,5 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-# Load data
-df = pd.read_csv('data/students.csv')
-
-# Preview
-print("Dataset Preview:")
-print(df.head())
-
-# Average scores
-print("\nAverage Scores:")
-print(df[['math_score', 'reading_score', 'writing_score']].mean())
-
-# By gender
-print("\nAverage by Gender:")
-print(df.groupby('gender').mean())
-
-# Correlation
-print("\nCorrelation:")
-print(df.corr())
-
-df[['math_score', 'reading_score', 'writing_score']].hist(bins=15)
-plt.suptitle("Distribution of Scores")
-plt.show()
-
-df.groupby('gender')[['math_score', 'reading_score', 'writing_score']].mean().plot(kind='bar')
-plt.title("Average Scores by Gender")
-plt.show()
-
-sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-plt.title("Correlation Matrix")
-plt.show()
-
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4], [10,20,25,30])
-plt.title("Test Graph")
+plt.plot([1, 2, 3], [10, 20, 30])
+plt.title("TEST GRAPH")
 plt.show()
-
