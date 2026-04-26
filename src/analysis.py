f3623d5b26752565c519e3ba21a5dd4b84769598
Deffoina <inamoting001@gmail.com>
2026-04-26 16:38:17 +0200

    analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('data/students.csv')
 
@@ -32,3 +33,12 @@ plt.ylabel("Score")
 plt.show()
 
 print("\nAnalysis completed successfully!")
+
+# -------------------------
+#  CORRELATION HEATMAP
+# -------------------------
+
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+
+plt.title("Correlation Between Student Scores")
+plt.show()
